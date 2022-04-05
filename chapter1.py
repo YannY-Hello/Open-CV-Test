33a7c28926a3306f57bcede374c3d3cd7c08773a
Yan Carlos <yanmoraes.ym40@gmail.com>
2022-04-05 15:20:42 -0400

    Atualizado o endereço do protocolo da camera interna!

--- chapter1.py
+++ chapter1.py
@@ -4,7 +4,7 @@ import cv2
 frameWidth = 640
 frameHeight = 480
 # Webcam, Mobile Cam in others
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('rtsp://teste:1892893894ZFZ@192.168.12.228:2808/cam/realmonitor?channel=17&subtype=0')
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
@@ -13,3 +13,4 @@ while True:
     cv2.imshow("Result", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
